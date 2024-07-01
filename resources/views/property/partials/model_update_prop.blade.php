3d9eac865d3a9f1c454106e402439d22bbc83947
Danish Hussain <danishhussain9525@hotmail.com>
2024-07-01 14:00:00 +0500

    changes

--- resources/views/property/partials/model_update_prop.blade.php
+++ resources/views/property/partials/model_update_prop.blade.php
@@ -5,24 +5,28 @@
   </select>
 </div>
 
-<div class="col-md-6 form-group">
-  <label class="control-label">{{$data['prop_type']}} No.</label>
-  <input type="number" value="{{$data['prop_number']}}" name="prop_number" placeholder="Enter property number here" class="form-control full_width">
-</div>
+@if ($data['prop_status'] == 'Available')
+  <div class="col-md-6 form-group">
+    <label class="control-label">{{$data['prop_type']}} No.</label>
+    <input type="number" value="{{$data['prop_number']}}" name="prop_number" placeholder="Enter property number here" class="form-control full_width">
+  </div>
+@endif
 
 @php $all_floors = get_floors(); @endphp
-@if ($data['prop_type'] != 'Bed Space')
-<div class="col-md-6 form-group">
-  <label class="control-label">Floor No.</label>
-  <select class="select2_field form-control full_width" id="prop_floor" name="prop_floor">
-    <option value=""> ---- Choose any option ---- </option>
-    @if (isset($all_floors) && count($all_floors) > 0 )
-      @foreach ($all_floors as $key => $prop_floor)
-        <option {{ old('prop_floor') == $prop_floor || (isset($data->prop_floor) && $data->prop_floor == $prop_floor) ? 'selected': '' }} value="{{$prop_floor}}"> {{$prop_floor}} </option>
-      @endforeach
-    @endif
-  </select>
-</div>
+@if ($data['prop_status'] == 'Available')
+  @if ($data['prop_type'] != 'Bed Space')
+  <div class="col-md-6 form-group">
+    <label class="control-label">Floor No.</label>
+    <select class="select2_field form-control full_width" id="prop_floor" name="prop_floor">
+      <option value=""> ---- Choose any option ---- </option>
+      @if (isset($all_floors) && count($all_floors) > 0 )
+        @foreach ($all_floors as $key => $prop_floor)
+          <option {{ old('prop_floor') == $prop_floor || (isset($data->prop_floor) && $data->prop_floor == $prop_floor) ? 'selected': '' }} value="{{$prop_floor}}"> {{$prop_floor}} </option>
+        @endforeach
+      @endif
+    </select>
+  </div>
+  @endif
 @endif
 
 <div class="col-md-6 form-group">
@@ -30,11 +34,13 @@
   <input type="number" value="{{$data['prop_rent']}}" name="prop_rent" placeholder="Enter property rental cost" class="form-control full_width">
 </div>
 
-@if ($data['prop_type'] != 'Bed Space')
-<div class="col-md-6 form-group">
-  <label class="control-label">Property Address</label>
-  <input type="text" value="{{$data['prop_address']}}" name="prop_address" placeholder="Enter property address" class="form-control full_width">
-</div>
+@if ($data['prop_status'] == 'Available')
+  @if ($data['prop_type'] != 'Bed Space')
+  <div class="col-md-6 form-group">
+    <label class="control-label">Property Address</label>
+    <input type="text" value="{{$data['prop_address']}}" name="prop_address" placeholder="Enter property address" class="form-control full_width">
+  </div>
+  @endif
 @endif
 
 <div class="col-md-12 form-group user-form-group">
