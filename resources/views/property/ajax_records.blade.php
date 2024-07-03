e98d36c544c5c522cc6bcda22f6f95787061ecda
Danish Hussain <danishhussain9525@hotmail.com>
2024-07-03 14:00:00 +0500

    changes

--- resources/views/property/ajax_records.blade.php
+++ resources/views/property/ajax_records.blade.php
@@ -24,12 +24,7 @@
       
         @php
           $sr_no = $key + 1;
-          $label_class = get_label_class($item['status']);
-          //if ($item->reservations_data()->exists())
-          //  $reservations = true;
-          //else
-          //  $reservations = false;
-
+          $label_class = get_label_class($item['prop_status']);
           if ($data['records']->currentPage()>1) {
             $sr_no = ($data['records']->currentPage()-1)*$data['records']->perPage();
             $sr_no = $sr_no + $key + 1;
@@ -41,14 +36,12 @@
           <td>{{ isset($item['prop_number']) ? $item['prop_number'] : "N/A" }}</td>
           <td>{{ $item['prop_floor'] }}</td>
           <td>{{ $item['prop_rent'] }}</td>
-          <td><span class="{{$label_class}} label label-pill">{{$item['status']}}</span></td>
+          <td><span class="{{$label_class}} label label-pill">{{$item['prop_status']}}</span></td>
           <td>
-            @if (in_array($item['status'], ['Available']))
-              <button type="button" class="btn btn-add btn-sm update_property_btn mb-3" title="Update Property" data-prop_id="{{$item['id']}}" data-action_url="{{ url($data['route_name'].'/'.$item['id'])}}" data-toggle="modal" data-target="#"><i class="fa fa-pencil"></i></button>
+            <button type="button" class="btn btn-add btn-sm update_property_btn mb-3" title="Update Property" data-prop_id="{{$item['id']}}" data-action_url="{{ url($data['route_name'].'/'.$item['id'])}}" data-toggle="modal" data-target="#"><i class="fa fa-pencil"></i></button>
+            @if (in_array($item['prop_status'], ['Available']))
               <button type="button" class="btn btn-danger btn-sm delete_btn mb-3" title="Delete Property" data-delete_url="{{ url($data['route_name'].'/'.$item['id'])}}" data-toggle="modal" data-target="#del_property_popup"><i class="fa fa-trash-o"></i> </button>
               <button type="button" class="btn btn-violet btn-sm reservation_btn mb-3" title="Reservation" data-prop_id="{{$item['id']}}" data-action_url="{{ url($data['route_name'].'/'.$item['id'])}}" data-toggle="modal" data-target="#"><i class="fa fa-calendar-check-o"></i> </button>
-            @else
-              {{'N/A'}}
             @endif
           </td>
         </tr>
