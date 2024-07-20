a43c4f9853d3ab408bba0bb3259abb00a78417b0
Danish Hussain <danishhussain9525@hotmail.com>
2024-07-20 14:00:00 +0500

    changes

--- resources/views/property/add_edit.blade.php
+++ resources/views/property/add_edit.blade.php
@@ -49,7 +49,7 @@
               <div class="row" style="margin-left: 0px;">
                 <div class="form-group col-md-4 col-sm-6 is-required">
                   <label>Type</label>
-                  <select class="select2_field form-control @error('prop_type') is-invalid @enderror" id="prop_type" name="prop_type" required>
+                  <select class="select2_field form-control full_width @error('prop_type') is-invalid @enderror" id="prop_type" name="prop_type" required>
                     <option value=""> ---- Choose any option ---- </option>
                     @if (isset($data['prop_types']) && count($data['prop_types']) > 0 )
                       @foreach ($data['prop_types'] as $key => $type)
@@ -67,7 +67,7 @@
 
               <div class="form-group dy_prop_number col-md-4 col-sm-6 is-required">
                 <label id="property_type">Property No.</label>
-                <input type="number" name="prop_number" id="prop_number" value="{{old('prop_number', isset($data->prop_number)? $data->prop_number: '')}}" class="form-control @error('prop_number') is-invalid @enderror" placeholder="Enter property number or reference" required>
+                <input type="number" name="prop_number" id="prop_number" value="{{old('prop_number', isset($data->prop_number)? $data->prop_number: '')}}" class="form-control full_width @error('prop_number') is-invalid @enderror" placeholder="Enter property number or reference" required>
                 {{--@error('prop_number')
                   <span class="invalid-feedback" role="alert"> {{ $message }} </span>
                 @else
@@ -77,7 +77,7 @@
 
               <div class="form-group dy_room_no col-md-4 col-sm-6 is-required" style="display: none;">
                 <label id="property_type">Room No.</label>
-                <input type="number" name="room_no" id="room_no" value="{{old('room_no', isset($data->room_no)? $data->room_no: '')}}" class="form-control @error('room_no') is-invalid @enderror" placeholder="Enter property room number" required>
+                <input type="number" name="room_no" id="room_no" value="{{old('room_no', isset($data->room_no)? $data->room_no: '')}}" class="form-control full_width @error('room_no') is-invalid @enderror" placeholder="Enter property room number" required>
                 {{--@error('room_no')
                   <span class="invalid-feedback" role="alert"> {{ $message }} </span>
                 @else
@@ -88,7 +88,7 @@
               @php $all_floors = get_floors(); @endphp
               <div class="form-group dy_floor col-md-4 col-sm-6 is-required">
                 <label>Floor No.</label>
-                <select class="select2_field form-control @error('prop_floor') is-invalid @enderror" id="prop_floor" name="prop_floor" required>
+                <select class="select2_field form-control full_width @error('prop_floor') is-invalid @enderror" id="prop_floor" name="prop_floor" required>
                   <option value=""> ---- Choose any option ---- </option>
                   @if (isset($all_floors) && count($all_floors) > 0 )
                     @foreach ($all_floors as $key => $prop_floor)
@@ -105,8 +105,47 @@
 
               <div class="form-group dy_rent col-md-4 col-sm-6 is-required">
                 <label>Rent (AED)</label>
-                <input type="number" name="prop_rent" id="prop_rent" value="{{old('prop_rent', isset($data->prop_rent)? $data->prop_rent: '')}}" class="form-control @error('prop_rent') is-invalid @enderror" placeholder="Enter property rental cost" required>
+                <input type="text" name="prop_rent" id="prop_rent" value="{{old('prop_rent', isset($data->prop_rent)? $data->prop_rent: '')}}" class="form-control full_width only_numbers @error('prop_rent') is-invalid @enderror" placeholder="Enter property rental cost" required>
                 {{--@error('prop_rent')
+                  <span class="invalid-feedback" role="alert"> {{ $message }} </span>
+                @else
+                  <span class="invalid-feedback" role="alert"></span>
+                @enderror--}}
+              </div>
+
+              <div class="form-group dy_other_charges col-md-4 col-sm-6 is-required">
+                <label>Other Charges</label>
+                <select class="form-control full_width" id="other_charges" name="other_charges">
+                  <option value=""> ---- Choose any option ---- </option>
+                  <option value="No"> No </option>
+                  <option value="Yes"> Yes </option>
+                </select>
+              </div>
+
+              <div class="form-group dy_dewa_ch col-md-4 col-sm-6 is-required">
+                <label>DEWA Charges</label>
+                <input type="text" name="dewa_ch" id="dewa_ch" value="{{old('dewa_ch', isset($data->dewa_ch)? $data->dewa_ch: '')}}" class="form-control full_width only_numbers @error('dewa_ch') is-invalid @enderror" placeholder="Enter dewa charges" required>
+                {{--@error('dewa_ch')
+                  <span class="invalid-feedback" role="alert"> {{ $message }} </span>
+                @else
+                  <span class="invalid-feedback" role="alert"></span>
+                @enderror--}}
+              </div>
+
+              <div class="form-group dy_wifi_ch col-md-4 col-sm-6 is-required">
+                <label>Wifi Charges</label>
+                <input type="text" name="wifi_ch" id="wifi_ch" value="{{old('wifi_ch', isset($data->wifi_ch)? $data->wifi_ch: '')}}" class="form-control full_width only_numbers @error('wifi_ch') is-invalid @enderror" placeholder="Enter property rental cost" required>
+                {{--@error('wifi_ch')
+                  <span class="invalid-feedback" role="alert"> {{ $message }} </span>
+                @else
+                  <span class="invalid-feedback" role="alert"></span>
+                @enderror--}}
+              </div>
+
+              <div class="form-group dy_misc_ch col-md-4 col-sm-6 is-required">
+                <label>Misc. Charges</label>
+                <input type="text" name="misc_ch" id="misc_ch" value="{{old('misc_ch', isset($data->misc_ch)? $data->misc_ch: '')}}" class="form-control full_width only_numbers @error('misc_ch') is-invalid @enderror" placeholder="Enter property rental cost" required>
+                {{--@error('misc_ch')
                   <span class="invalid-feedback" role="alert"> {{ $message }} </span>
                 @else
                   <span class="invalid-feedback" role="alert"></span>
@@ -115,7 +154,7 @@
 
               <div class="form-group dy_prop_add col-md-4 col-sm-6 is-required">
                 <label>Property Address</label>
-                <input type="text" name="prop_address" id="prop_address" value="{{old('prop_address', isset($data->prop_address)? $data->prop_address: '')}}" class="form-control @error('prop_address') is-invalid @enderror" placeholder="Enter property address" required>
+                <input type="text" name="prop_address" id="prop_address" value="{{old('prop_address', isset($data->prop_address)? $data->prop_address: '')}}" class="form-control full_width @error('prop_address') is-invalid @enderror" placeholder="Enter property address" required>
                 {{--@error('prop_address')
                   <span class="invalid-feedback" role="alert"> {{ $message }} </span>
                 @else
@@ -126,7 +165,7 @@
               <div class="row" style="margin-left: 0px;">
                 <div class="form-group dy_bs_level col-md-4 col-sm-6 is-required">
                   <label>Level</label>
-                  <select class="form-control @error('bs_level') is-invalid @enderror" id="bs_level" name="bs_level" required>
+                  <select class="form-control full_width @error('bs_level') is-invalid @enderror" id="bs_level" name="bs_level" required>
                     <option value=""> ---- Choose any option ---- </option> 
                     <option {{ old('bs_level') == "1" || (isset($data->bs_level) && $data->bs_level == "1") ? 'selected': '' }} value="1"> Down </option>
                     <option {{ old('bs_level') == "2" || (isset($data->bs_level) && $data->bs_level == "2") ? 'selected': '' }} value="2"> 1st Up </option>
