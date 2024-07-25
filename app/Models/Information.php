863b243d60a3c7372748d00e584e57449c91213e
Danish Hussain <danishhussain9525@hotmail.com>
2024-07-25 14:00:00 +0500

    changes

--- app/Models/Information.php
+++ app/Models/Information.php
@@ -10,30 +10,8 @@ use Illuminate\Support\Facades\Hash;
 
 class Information extends Model
 {
-  use HasFactory, SoftDeletes;
+  use HasFactory;
   protected $table = 'informations';
-
-  public function booked_by_user()
-  {
-    return $this->belongsTo(User::class, 'booked_by');
-  }
-
-  public function booked_for_user()
-  {
-    return $this->belongsTo(User::class, 'booked_for');
-  }
-
-  public function property_data()
-  {
-    return $this->belongsTo(Property::class, 'property_id');
-  }
-
-  // public function skills() {
-  //   return $this->hasMany(InformationInformation::class, 'user_id', 'id')->with('skill_data')
-  //     ->where([
-  //         ['info_status', '=' ,'Skill'],
-  //     ])->select(['id', 'user_id', 'info_status', 'skill_id', 'created_at', 'updated_at']);
-  // }
 
   public function getInformation($posted_data = array())
   {
@@ -42,29 +20,13 @@ class Information extends Model
     $query = Information::latest();
 
     if (isset($posted_data['relations']) && $posted_data['relations']) {
-      $query = $query->with('booked_by_user')
-        ->with('booked_for_user')
-        ->with('property_data');
+      // $query = $query->with('booked_by_user')
+      //   ->with('booked_for_user')
+      //   ->with('property_data');
     }
 
     if (isset($posted_data['id'])) {
       $query = $query->where('informations.id', $posted_data['id']);
-    }
-    if (isset($posted_data['first_name'])) {
-      $query = $query->where('informations.first_name', 'like', '%' . $posted_data['first_name'] . '%');
-    }
-    if (isset($posted_data['last_name'])) {
-      $query = $query->where('informations.last_name', 'like', '%' . $posted_data['last_name'] . '%');
-    }
-    if (isset($posted_data['search_query'])) {
-      $str = $posted_data['search_query'];
-      $query = $query->where(
-        function ($query) use ($str) {
-          return $query
-            ->where('informations.prop_type', 'like', '%' . $str . '%')
-            ->orwhere('informations.prop_number', 'like', '%' . $str . '%');
-        }
-      );
     }
     if (isset($posted_data['status'])) {
       $query = $query->where('informations.status', $posted_data['status']);
@@ -125,56 +87,23 @@ class Information extends Model
       }
     }
 
-    if (isset($posted_data['booked_by'])) {
-      $data->booked_by = $posted_data['booked_by'];
+    if (isset($posted_data['c_name'])) {
+      $data->c_name = $posted_data['c_name'];
     }
-    if (isset($posted_data['booked_for'])) {
-      $data->booked_for = $posted_data['booked_for'];
+    if (isset($posted_data['c_email'])) {
+      $data->c_email = $posted_data['c_email'];
     }
-    if (isset($posted_data['property_id'])) {
-      $data->property_id = $posted_data['property_id'];
+    if (isset($posted_data['c_office_ph'])) {
+      $data->c_office_ph = $posted_data['c_office_ph'];
     }
-    if (isset($posted_data['checkin_date'])) {
-      $data->checkin_date = $posted_data['checkin_date'];
+    if (isset($posted_data['c_mobile_ph'])) {
+      $data->c_mobile_ph = $posted_data['c_mobile_ph'];
     }
-    if (isset($posted_data['checkout_date'])) {
-      $data->checkout_date = $posted_data['checkout_date'];
+    if (isset($posted_data['c_address'])) {
+      $data->c_address = $posted_data['c_address'];
     }
-    if (isset($posted_data['for_days'])) {
-      $data->for_days = $posted_data['for_days'];
-    }
-    if (isset($posted_data['for_months'])) {
-      $data->for_months = $posted_data['for_months'];
-    }
-    if (isset($posted_data['rent'])) {
-      $data->rent = $posted_data['rent'];
-    }
-    if (isset($posted_data['adjust_rent'])) {
-      $data->adjust_rent = $posted_data['adjust_rent'];
-    }
-    if (isset($posted_data['grace_rent'])) {
-      $data->grace_rent = $posted_data['grace_rent'];
-    }
-    if (isset($posted_data['other_charges'])) {
-      $data->other_charges = $posted_data['other_charges'];
-    }
-    if (isset($posted_data['dewa_charges'])) {
-      $data->dewa_charges = $posted_data['dewa_charges'];
-    }
-    if (isset($posted_data['wifi_charges'])) {
-      $data->wifi_charges = $posted_data['wifi_charges'];
-    }
-    if (isset($posted_data['admin_charges'])) {
-      $data->admin_charges = $posted_data['admin_charges'];
-    }
-    if (isset($posted_data['security_charges'])) {
-      $data->security_charges = $posted_data['security_charges'];
-    }
-    if (isset($posted_data['net_total'])) {
-      $data->net_total = $posted_data['net_total'];
-    }
-    if (isset($posted_data['deleted_at'])) {
-      $data->deleted_at = $posted_data['deleted_at'];
+    if (isset($posted_data['c_logo'])) {
+      $data->c_logo = $posted_data['c_logo'];
     }
     $data->save();
 
