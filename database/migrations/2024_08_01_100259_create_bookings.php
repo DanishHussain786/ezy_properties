ae51db93a49e07c4919d208d9346a8c30230bf93
Danish Hussain <danishhussain9525@hotmail.com>
2024-08-01 20:22:18 +0500

    new migration

---
+++ database/migrations/2024_08_01_100259_create_bookings.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('bookings', function (Blueprint $table) {
+      $table->id();
+      $table->unsignedBigInteger('booked_by')->nullable();
+      $table->foreign('booked_by')->references('id')->constrained()->on('users')->onUpdate('cascade')->onDelete('cascade');
+      $table->unsignedBigInteger('booked_for')->nullable();
+      $table->foreign('booked_for')->references('id')->constrained()->on('users')->onUpdate('cascade')->onDelete('cascade');
+      $table->unsignedBigInteger('property_id')->nullable();
+      $table->foreign('property_id')->references('id')->constrained()->on('properties')->onUpdate('cascade')->onDelete('cascade');
+      $table->enum('status', ['Reservation','CheckIn','CheckOut','OverStay'])->default('Reservation');
+      $table->date('checkin_date');
+      $table->date('checkout_date');
+      $table->integer('no_of_days')->nullable();
+      $table->double('rent')->nullable();
+      $table->double('grace_rent')->nullable();
+      $table->double('dewa_charges')->nullable();
+      $table->double('wifi_charges')->nullable();
+      $table->double('admin_charges')->nullable();
+      $table->double('security_charges')->nullable();
+      $table->double('net_total')->nullable();
+      $table->softDeletes('deleted_at');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('bookings');
+  }
+};
