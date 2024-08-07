7052b03e9add1d6bd7dff1843f44019935b25878
Danish Hussain <danishhussain9525@hotmail.com>
2024-08-07 14:00:00 +0500

    changes

--- resources/views/account/list.blade.php
+++ resources/views/account/list.blade.php
@@ -20,20 +20,37 @@
 </section>
 
 <style>
-  .summary-row {
-    display: grid;
-    grid-template-columns: 1fr auto;
-    gap: 10px;
+  .receipt {
+    margin: 20px;
   }
 
-  .summary-label {
-    text-align: left;
-    margin-bottom: 3px;
-  }
-
-  .summary-value {
+  .table .table-styles th,
+  .table .table-styles td {
     text-align: right;
+  }
+
+  .table .table-styles th {
+    text-align: left;
+  }
+
+  .terms {
+    margin-top: 20px;
+  }
+
+  .guest-details {
+    margin-bottom: 20px;
+  }
+
+  .guest-details .row {
     margin-bottom: 3px;
+  }
+
+  .guest-details .col-xs-3 {
+    padding-right: 0;
+  }
+
+  .guest-details .col-xs-9 {
+    padding-left: 0;
   }
 </style>
 
@@ -122,140 +139,8 @@
   <!-- /.Modal -->
 
   <!-- Modal -->
-  <div class="modal fade" id="del_reservation_popup" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header modal-header-primary">
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-          <h3><i class="fa fa-user m-r-5"></i> Delete Reservation</h3>
-        </div>
-        <div class="modal-body">
-          <div class="row">
-            <div class="col-md-12">
-              <form class="form-horizontal delete_popup" action="" method="POST">
-                @method('DELETE')
-                @csrf
-                <fieldset>
-                  <div class="col-md-12 form-group user-form-group">
-                    <label style="font-weight: 500;" class="control-label">Are you sure to delete reservation?</label>
-                    <div class="pull-right">
-                      <button type="button" class="btn btn-danger btn-sm">No</button>
-                      <button type="submit" class="btn btn-add btn-sm">Yes</button>
-                    </div>
-                  </div>
-                </fieldset>
-              </form>
-            </div>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-danger pull-right" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-      <!-- /.modal-content -->
-    </div>
-    <!-- /.modal-dialog -->
-  </div>
-  <!-- /.modal -->
-
-  <!-- Modal -->
-  <div class="modal fade" id="checkin_popup" role="dialog" aria-hidden="true">
+  <div class="modal fade" id="payables_popup" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-lg">
-      <div class="modal-content">
-        <div class="modal-header modal-header-primary">
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-          <h3><i class="fa fa-user m-r-5"></i> Check-In Details</h3>
-        </div>
-        <div class="modal-body">
-          <div class="row">
-            <div class="col-md-12">
-              <form class="form-horizontal" id="create_checkin" action="" method="">
-                @csrf
-                <fieldset>
-                  <input type="hidden" value="" id="book_id" name="book_id">
-                  <input type="hidden" value="" id="prop_id" name="prop_id">
-                  <input type="hidden" value="" id="user_id" name="user_id">
-                  <div class="form-group col-md-4 col-sm-6 col-xs-12">
-                    <label class="control-label">Charges</label>
-                    <input type="number" value="" name="tot_payable" placeholder="Enter payable charges" class="form-control full_width tot_payable" readonly>
-                  </div>
-                  <div class="form-group col-md-4 col-sm-6 col-xs-12">
-                    <label class="control-label">VAT (5%)</label>
-                    <select class="form-control full_width" id="vat_apply" name="vat_apply">
-                      <option value="No" selected> No </option>
-                      <option value="Inclusive"> Inclusive </option>
-                      <option value="Exclusive"> Exclusive </option>
-                    </select>
-                  </div>
-                  <div class="form-group col-md-4 col-sm-6 col-xs-12">
-                    <label class="control-label">Discount</label>
-                    <input type="text" value="" id="discount" name="discount" placeholder="Enter discount amount" class="form-control full_width only_numbers">
-                  </div>
-                  <div class="form-group col-md-4 col-sm-6 col-xs-12">
-                    <label class="control-label">Pay Method</label>
-                    <select class="form-control full_width" id="pay_with" name="pay_with">
-                      <option value="Cash" selected> Cash </option>
-                      <option value="Online"> Online </option>
-                      <option value="Credit-Card"> Credit Card </option>
-                      <option value="Bank-Transfer"> Bank Transfer </option>
-                      <option value="Cheque"> Cheque </option>
-                    </select>
-                  </div>
-                  <div class="form-group col-md-4 col-sm-6 col-xs-12">
-                    <label class="control-label">Amount Paid</label>
-                    <input type="text" value="" id="amt_pay" name="amt_pay" placeholder="Enter total amount paid" class="form-control full_width only_numbers">
-                  </div>
-                  <div class="form-group col-md-4 col-sm-6 col-xs-12">
-                    <label class="control-label">Any comments</label>
-                    <input type="text" value="" id="comments" name="comments" placeholder="Enter any description or comments" class="form-control full_width">
-                  </div>
-
-                  <div class="col-md-7"></div>
-                  <div class="col-md-5 text-right" style="padding-top: 15px; display: grid;">
-                    <div class="summary-row">
-                      <p class="summary-label"><strong>Sub Total:</strong></p>
-                      <p class="summary-value" id="sum_st">0</p>
-                    </div>
-                    <div class="summary-row">
-                      <p class="summary-label"><strong>VAT (5%):</strong></p>
-                      <p class="summary-value" id="sum_vat">0</p>
-                    </div>
-                    <div class="summary-row">
-                      <p class="summary-label"><strong>Discount:</strong></p>
-                      <p class="summary-value" id="sum_disc">0</p>
-                    </div>
-                    <div class="summary-row">
-                      <p class="summary-label"><strong>Grand Total:</strong></p>
-                      <p class="summary-value" id="sum_gt">0</p>
-                    </div>
-                    <div class="summary-row">
-                      <p class="summary-label"><strong>Total Paid:</strong></p>
-                      <p class="summary-value" id="sum_tp">0</p>
-                    </div>
-                    <div class="summary-row">
-                      <p class="summary-label"><strong>Balance:</strong></p>
-                      <p class="summary-value" id="sum_bal">0</p>
-                    </div>
-                  </div>
-                </fieldset>
-              </form>
-            </div>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <div class="pull-right">
-            <button type="submit" id="do_checkin_btn" class="btn btn-add">Save</button>
-            <button type="button" class="btn btn-danger pull-right" data-dismiss="modal">Close</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- /.Modal -->
-
-  <!-- Modal -->
-  <div class="modal fade" id="payment_popup" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header modal-header-primary">
           <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
@@ -266,32 +151,7 @@
             <div class="col-md-12">
               <form class="form-horizontal" id="add_payment_form" action="{{ url('booking/payments') }}" method="POST">
                 @csrf
-                <fieldset>
-                  <input type="hidden" value="initial_dep" id="paying_for" name="paying_for">
-                  <!-- <input type="hidden" value="" id="prop_id" name="prop_id">
-                  <input type="hidden" value="" id="user_id" name="user_id"> -->
-                  <div class="form-group col-md-6 col-sm-6 col-xs-12">
-                    <label class="control-label">Charges</label>
-                    <input type="number" value="" name="tot_payable" placeholder="Enter payable charges" class="form-control full_width tot_payable" readonly>
-                  </div>
-                  <div class="form-group col-md-6 col-sm-6 col-xs-12">
-                    <label class="control-label">Pay Method</label>
-                    <select class="form-control full_width" id="pay_with" name="pay_with">
-                      <option value="Cash" selected> Cash </option>
-                      <option value="Online"> Online </option>
-                      <option value="Credit-Card"> Credit Card </option>
-                      <option value="Bank-Transfer"> Bank Transfer </option>
-                      <option value="Cheque"> Cheque </option>
-                    </select>
-                  </div>
-                  <div class="form-group col-md-6 col-sm-6 col-xs-12">
-                    <label class="control-label">Initial Deposit</label>
-                    <input type="text" value="" id="amt_pay" name="amt_pay" placeholder="Enter total amount paid" class="form-control full_width only_numbers">
-                  </div>
-                  <div class="form-group col-md-6 col-sm-6 col-xs-12">
-                    <label class="control-label">Any comments</label>
-                    <input type="text" value="" id="comments" name="comments" placeholder="Enter any description or comments" class="form-control full_width">
-                  </div>
+                <fieldset class="model-ajax-receipts">
                 </fieldset>
               </form>
             </div>
