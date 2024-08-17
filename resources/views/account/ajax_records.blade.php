2b91dd40db283c37517320d9ac67324ae6255ba6
Danish Hussain <danishhussain9525@hotmail.com>
2024-08-17 14:00:00 +0500

    changes

--- resources/views/account/ajax_records.blade.php
+++ resources/views/account/ajax_records.blade.php
@@ -5,9 +5,6 @@
         <th>Sr #</th>
         <th>Booking Id</th>
         <th>Guest Name</th>
-        <th>Check In/Out</th>
-        <th>Property</th>
-        <th>Others</th>
         <th>Balance</th>
         <th>Total<br>Payable</th>
         <th>Action</th>
@@ -38,33 +35,14 @@
           <td>{{ $sr_no }}</td>
           <td>{{ $item['booked_id'] }}</td>
           <td>{{ $item->booked_for_user->first_name }} {{ $item->booked_for_user->last_name }}</td>
-          <td>
-            <strong class="font-sm">Check In: </strong>{{ $item['checkin_date'] }}<br>
-            <strong class="font-sm">Check Out: </strong>{{ $item['checkout_date'] }}
-          </td>
-          <td>
-            <strong class="font-sm">Type: </strong>{{ default_value($item->property_data->prop_type, "str") }}<br>
-            <strong class="font-sm">No: </strong>{{ default_value($item->property_data->prop_number, "str") }}<br>
-            <strong class="font-sm">Floor: </strong>{{ default_value($item->property_data->prop_floor, "str") }}<br>
-            <strong class="font-sm">Rent: </strong>{{ default_value($item->property_data->prop_rent, "str") }}
-          </td>
-          <td>
-            <strong class="font-sm">Markup Rent: </strong>{{ default_value($item['markup_rent'], "num") }}<br>
-            <strong class="font-sm">Dewa Charges: </strong>{{ default_value($item['dewa_charges'], "num") }}<br>
-            <strong class="font-sm">Wifi Charges: </strong>{{ default_value($item['wifi_charges'], "num") }}<br>
-            <strong class="font-sm">Admin Charges: </strong>{{ default_value($item['admin_charges'], "num") }}<br>
-            <strong class="font-sm">Security Charges: </strong>{{ default_value($item['security_charges'], "num") }}
-          </td>
           <td>{{ $item['balance'] }}</td>
           <td>{{ $item['total_payable'] }}</td>
           <td>
-            {{-- @if ($init_dep == 0) --}}
-            <button type="button" class="btn btn-add btn-sm update_reservation_btn mb-3" title="Update Reservation" data-item_id="{{$item['id']}}" data-action_url="{{ url($data['route_name'].'/'.$item['id'])}}" data-toggle="modal" data-target="#"><i class="fa fa-pencil"></i></button>
-            {{-- @endif --}}
+            <button type="button" class="btn btn-add btn-sm update_reservation_btn mb-3" title="Update Reservation" data-item_id="{{$item['id']}}" data-action_url="{{ url($data['route_name'].'/'.$item['id'])}}" data-toggle="modal" data-target="#"><i class="fa fa-eye"></i></button>
 
-            {{-- @if ($init_dep == 0) --}}
+            {{--@if ($init_dep == 0)--}}
             <button type="button" class="btn btn-danger btn-sm delete_btn mb-3" title="Delete Reservation" data-delete_url="{{ url($data['route_name'].'/'.$item['id'])}}" data-toggle="modal" data-target="#del_reservation_popup"><i class="fa fa-trash-o"></i> </button>
-            {{-- @endif --}}
+            {{--@endif--}}
 
             <button type="button" class="btn btn-purple btn-sm checkin_btn mb-3" title="Check In" data-item_id="{{$item['id']}}" data-metadata="porp={{$item->property_data->id}},resu={{$item->booked_for_user->id}},latot={{$item['total_payable']}}"  data-toggle="modal" data-target="#"><i class="fa fa-calendar-plus-o"></i> </button>
 
