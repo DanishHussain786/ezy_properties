74a02dbc1c6a172b25a991fa9b2ddbbf3b50dad5
Danish Hussain <danishhussain9525@hotmail.com>
2024-08-18 14:00:00 +0500

    changes

--- database/migrations/2024_07_21_158895_create_properties_table.php
+++ database/migrations/2024_07_21_158895_create_properties_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
       $table->string('prop_number', 30)->nullable();
       $table->string('prop_floor', 30)->nullable();
       $table->double('prop_rent');
+      $table->enum('other_charges', ['No','Yes'])->default('No');
+      $table->double('dewa_charges')->nullable();
+      $table->double('wifi_charges')->nullable();
+      $table->double('misc_charges')->nullable();
       $table->longText('prop_address')->nullable();
       $table->enum('prop_status', ['Available','Pre-Reserve','Reserved','Checked-In','Checked-Out','Maintenance','Over-Stay'])->default('Available');
       $table->softDeletes('deleted_at');
