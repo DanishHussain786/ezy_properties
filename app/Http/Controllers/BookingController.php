a7eb8142b959aaf2dfb5a686b998a0aa2ad77ab5
Danish Hussain <danishhussain9525@hotmail.com>
2024-08-18 14:00:00 +0500

    changes

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -217,7 +217,7 @@ class BookingController extends Controller
 		$data['redirect_url'] = url('property');
 
 		$property = $this->PropertyObj->getProperty(['id' => $request_data['property_id'], 'detail' => true]);
-		$property->status = 'Pre-Reserve';
+		$property->prop_status = 'Pre-Reserve';
 		$property->save();
 
 		// if ($deposit > 0) {
@@ -244,29 +244,24 @@ class BookingController extends Controller
 	 */
 	public function show(Request $request, $id = 0)
 	{
-		// $id = \Crypt::decrypt($id); 
-		// $request_data = $request->all();		
-		// $posted_data = array();
+		$request_data = $request->all();
+		if ($id != 0)
+			$posted_data['id'] = $id;
 
-		// if ($id != 0)
-		// 	$posted_data['id'] = $id;
-
-		// $posted_data['detail'] = true;
-		// $request_data = array_merge($request_data,$posted_data);
-		// $data = $this->BookingObj->getBooking($request_data);
+		$posted_data['detail'] = true;
+		$request_data = array_merge($request_data, $posted_data);
+		$request_data['relations'] = true;
+		$data = $this->BookingObj->getBooking($request_data);
 		// $data['users'] = $this->UserObj->getUser(['role' => 'Guest']);
 		// $data['route_name'] = $this->route_name;
 
-		// if (isset($request_data['return_to']) && $request_data['return_to'] == 'model_reservation') {
-		// 	$data['html'] = view("{$this->route_name}.partials.model_reservation", compact('data'));
-		// }
+		if (isset($request_data['return_to']) && $request_data['return_to'] == 'model_payables') {
+			$data['html'] = view("account.partials.model_payables", compact('data'));
+		}
 
-		// if ($request->ajax()) {
-		// 	return $data['html'];
-		// }
-
-		// print_r($data);
-		// exit();
+		if ($request->ajax()) {
+			return $data['html'];
+		}
 	}
 
 	/**
